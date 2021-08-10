698e7c038714e21cb5d3976edd6464e3c0a1abc2
dillonupgradeit <dillon@upgrade-sf.com>
2021-08-10 09:25:29 -0700

    Fixed message sort order

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,9 +19,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [[Message, "createdAt", "ASC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message },
         {
           model: User,
           as: "user1",
