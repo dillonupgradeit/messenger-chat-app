3a0df25762e234570a25cbf477b4a6c6aa4491d3
dillonupgradeit <dillon@upgrade-sf.com>
2021-08-18 22:33:33 -0700

    Removed last-read

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,6 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
-const LastRead = require("./last-read");
 
 // associations
 
@@ -10,15 +9,9 @@ Conversation.belongsTo(User, { as: "user1" });
 Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
-LastRead.belongsTo(Conversation);
-LastRead.belongsTo(User);
-LastRead.belongsTo(Message);
-Conversation.hasMany(LastRead);
-Message.hasMany(LastRead);
 
 module.exports = {
   User,
   Conversation,
   Message,
-  LastRead
 };
