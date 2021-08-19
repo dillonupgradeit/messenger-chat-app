edb4d9dda37b31eb5c9e67a08aa9d317aff15ef0
dillonupgradeit <dillon@upgrade-sf.com>
2021-08-18 23:06:40 -0700

    Refactored socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,10 +4,16 @@ import {
   setNewMessage,
   removeOfflineUser,
   addOnlineUser,
-  setNewReads,
+  updateMessageRead,
+  increaseUnreadCount,
 } from "./store/conversations";
+import { updateLastReadMessage } from "./store/utils/thunkCreators";
 
 const socket = io(window.location.origin);
+
+export const setActiveChatInSocket = (activeChat) => {
+  socket.activeChat = activeChat;
+}
 
 socket.on("connect", () => {
   console.log("connected to server");
@@ -22,11 +28,18 @@ socket.on("connect", () => {
   
   socket.on("new-message", (data) => {
     store.dispatch(setNewMessage(data.message, data.sender));
+    if(data.senderName === socket.activeChat){
+      // update conversation reads from current user
+      store.dispatch(updateLastReadMessage(data));
+    } else {
+      store.dispatch(increaseUnreadCount(data.message));
+    }
   });
 
-  // update state with new reads
-  socket.on("update-read", (data) => {
-    store.dispatch(setNewReads(data.lastReads))
+  socket.on("last-read-message", (data) => {
+    // update conversations in store from other user
+    // 2nd parameter "true" signifies that the updateMessageRead is being passed from another user.
+    store.dispatch(updateMessageRead(data.message, true));
   });
 
 });
