1f43af3715e9354ac71edf418429ccdcf41fb67e
dillonupgradeit <dillon@upgrade-sf.com>
2021-08-21 19:04:28 -0700

    Refactored data model to include Participant

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,24 +1,21 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
-const LastRead = require("./last-read");
+const Participant = require("./participant");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
-Message.belongsTo(Conversation);
+Conversation.belongsToMany(User, { through: Participant });
+Conversation.belongsToMany(User, {through: Participant, as: 'filterUser'});
 Conversation.hasMany(Message);
-LastRead.belongsTo(Conversation);
-LastRead.belongsTo(User);
-LastRead.belongsTo(Message);
-Conversation.hasMany(LastRead);
-Message.hasMany(LastRead);
+Conversation.hasMany(Participant);
+Message.belongsTo(Conversation);
+Message.hasMany(Participant, { foreignKey: "lastRead" });
+
 
 module.exports = {
   User,
   Conversation,
   Message,
-  LastRead
+  Participant
 };
