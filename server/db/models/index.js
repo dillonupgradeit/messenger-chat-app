b124eb716b1f2ba89fe8c75f118c7ea725f73b5f
dillonupgradeit <dillon@upgrade-sf.com>
2021-08-23 14:35:00 -0700

    Refactored data model index to include participant

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,20 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Participant = require("./participant");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
-Message.belongsTo(Conversation);
+Conversation.belongsToMany(User, { through: Participant });
+Conversation.belongsToMany(User, {through: Participant, as: 'filterUser'});
 Conversation.hasMany(Message);
+Conversation.hasMany(Participant);
+Message.belongsTo(Conversation);
+Message.hasMany(Participant, { foreignKey: "lastRead" });
 
 module.exports = {
   User,
   Conversation,
   Message,
+  Participant
 };
