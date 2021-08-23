d9bd3425c64f6e4f8c9f22627f49dd252089ca4d
dillonupgradeit <dillon@upgrade-sf.com>
2021-08-23 14:36:13 -0700

    Added v2 functions to conversation model

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,11 +1,17 @@
 const { Op } = require("sequelize");
 const db = require("../db");
-const Message = require("./message");
+const User = require("./user");
+const Participant = require("./participant");
+const Sequelize = require("sequelize");
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define("conversation", { 
+  nickname: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  } 
+},{ userId: false });
 
-// find conversation given two user Ids
-
+// (v1) find conversation given two user Ids
 Conversation.findConversation = async function (user1Id, user2Id) {
   const conversation = await Conversation.findOne({
     where: {
@@ -14,8 +20,82 @@ Conversation.findConversation = async function (user1Id, user2Id) {
       },
       user2Id: {
         [Op.or]: [user1Id, user2Id]
-      }
+      },
     }
+  });
+
+  // return conversation or null if it doesn't exist
+  return conversation;
+};
+
+// (v2) : get conversation by conversationId
+Conversation.findConversationById = async function (conversationId) {
+  const conversation = await Conversation.findOne({
+    where: {
+      id: conversationId 
+    }
+  });
+
+  // return conversation or null if it doesn't exist
+  return conversation;
+};
+
+// (v2) : get conversation by conversationId and participant userId
+Conversation.findConversationByIdAndUserId = async function (conversationId, userId) {
+  const conversation = await Conversation.findOne({
+    where: {
+      id: conversationId
+    },
+    include: [
+      {
+        model: Participant,
+        where: {
+          userId: userId
+        },
+        required: true
+
+      }
+    ],
+    attributes: ["id"]
+  });
+
+  // return conversation or null if it doesn't exist
+  return conversation;
+};
+
+// (v2) find conversation by conversation nickname or name of user in conversation
+Conversation.findConversationBySearchTerm = async function (searchTerm) {
+  const conversation = await Conversation.findAll({
+    include: [
+      { 
+        model: User,
+        as: "filterUser",
+        where: {
+          [Op.or]: [
+            {
+              username: {
+                [Op.iLike]: `%${searchTerm}%`
+              }
+            },
+            {
+              '$conversation.nickname$' : {
+                [Op.iLike]: `%${searchTerm}%`
+              } 
+            }
+          ]
+        },
+        attributes: [] 
+      },
+      { 
+        model: User,
+        through: {
+            attributes: []
+        },
+        attributes: ["id","username", "photoUrl"] 
+      }
+    ],
+    attributes: ["id", "nickname"],
+    required: true
   });
 
   // return conversation or null if it doesn't exist
